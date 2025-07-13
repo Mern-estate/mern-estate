962c10bcd6f153b4b2b9fce9049632cdf455a870
Saraswati Chaudhary <saru.chaudhary20018@gmail.com>
2025-07-13 09:41:22 +0545

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotevn from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotevn.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mangodb');
@@ -13,3 +14,5 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 }
 );
+
+app.use('/api/user', userRouter);
