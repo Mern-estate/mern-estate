4ea33068abf7940c27b3c753b8c4e291aa6b23f8
Saraswati Chaudhary <saru.chaudhary20018@gmail.com>
2025-07-19 22:39:38 +0545

    complete update user functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-8d23f.firebaseapp.com",
-  projectId: "mern-estate-8d23f",
-  storageBucket: "mern-estate-8d23f.firebasestorage.app",
-  messagingSenderId: "95617465677",
-  appId: "1:95617465677:web:6f3bb0eec623352e6a8b04"
+  authDomain: "mern-estate-2dcd6.firebaseapp.com",
+  projectId: "mern-estate-2dcd6",
+  storageBucket: "mern-estate-2dcd6.firebasestorage.app",
+  messagingSenderId: "604616099630",
+  appId: "1:604616099630:web:1a805eba19d58da0e79cbf"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
+
