ca1a977cc5824925596478e3dec877ee8a9cb5b8
Saraswati Chaudhary <saru.chaudhary20018@gmail.com>
2025-07-19 10:16:15 +0545

    create user update api

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotevn from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth .route.js'
+import authRouter from './routes/auth .route.js';
+import cookieParser from 'cookie-parser';
 dotevn.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mangodb');
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 }
